3693665021cd1aaaf0201666b99973a472939626
Peter Loos <peter.loos@gmx.de>
2026-04-13 15:37:30 +0200

    Add project files.

---
+++ GeneralSnippets/RValueLValue/RValueLValue.cpp
@@ -0,0 +1,92 @@
+// =====================================================================================
+// LValue / RValue
+// =====================================================================================
+
+module modern_cpp:rvalue_lvalue;
+
+namespace LValueRValue {
+
+    // lvalue reference
+    static void sayHello(/*const*/ std::string& message) {
+        std::println("sayHello [std::string&]:  {}", message);
+    }
+
+    // rvalue reference
+    static void sayHello(std::string&& message) {
+        std::println("sayHello [std::string&&]: {}", message);
+    }
+
+    static void test01() {
+
+        std::string a = "Hello";
+        //std::string& ra;  // ALIAS // Name of an Object
+
+        std::string b = " World";
+
+        sayHello(a);
+
+        sayHello(a + b);  // unnamed object
+                          // temporary objects
+    }
+
+    // -------------------------------------------------------------------
+
+    static void helper(std::string&& message)
+    {
+        sayHello(std::move(message)); // &&: types are identical
+                           // &:  parameter has a name
+    }
+
+    static void test02()
+    {
+        std::string a("ABC");
+
+        // helper(a);
+
+        helper(std::string("Where are we going ..."));
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test03() {
+
+        std::string s = "Hello";
+
+        sayHello(s);
+        // versus
+        sayHello(std::move(s));  // casts an lvalue to an rvalue
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test04() {
+
+        int a = 2;
+        int b = 3;
+
+        int& ri = a;          // works: (lvalue) reference to a (named) variable
+
+        // int& i = 123;      // invalid: (lvalue) reference to a constant
+
+        int&& i = 123;        // works: (rvalue) reference to a constant
+
+        const int& j = 123;   // works: const references binds to everything
+
+        // int& k = a + b;    // invalid: (lvalue) reference to a temporary object
+
+        int&& k = a + b;      // works: (rvalue) reference to a temporary object
+    }
+}
+
+void main_rvalue_lvalue()
+{
+    using namespace LValueRValue;
+    test01();
+    test02();
+    test03();
+    test04();
+}
+
+// =====================================================================================
+// End-of-File
+// =====================================================================================
